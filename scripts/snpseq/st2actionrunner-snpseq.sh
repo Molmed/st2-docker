0a6c517a07f1c6b9c32f7c90e37ceec61c5ae510
matrulda <matilda.aslin@medsci.uu.se>
2021-09-20 12:24:07 +0200

    Add genologics config to st2actionrunner container

--- scripts/snpseq/st2actionrunner-snpseq.sh
+++ scripts/snpseq/st2actionrunner-snpseq.sh
@@ -2,3 +2,6 @@
 
 # Install dependencies
 apt-get update && apt-get install -y gcc python
+
+## Copy dummy genologics config to home of root user (required for some snpseq_packs actions)
+cp /opt/stackstorm/packs/snpseq_packs/utils/.genologicsrc /root/
